ad829885d301d350eda623bce99031b086978d78
Arnav Sharma <sharma55@wisc.edu>
2024-03-29 09:12:48 -0500

    Script to add ProteinGym repo as a submodule

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+# Setup ProteinGym Repo
 git submodule update --init --recursive
+
+# Download ProteinGym DMS data
+
+datasets=("DMS_ProteinGym_substitutions" "DMS_msa_files")
+
+for zipfile in "${datasets[@]}"
+do
+    if [ -e zipfile ]
+    then
+        echo "Found zip file! Skipping Download."
+    else
+        curl -o "$zipfile".zip https://marks.hms.harvard.edu/proteingym/"$zipfile".zip
+    fi
+    unzip -o "$zipfile".zip -d "$zipfile" && rm -f "$zipfile".zip
+done
